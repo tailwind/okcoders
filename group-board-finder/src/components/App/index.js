e469c7b5549b0d80b479006b86c351994c0e2823
David Scott <dscottjr99@yahoo.com>
2017-11-11 12:44:24 -0600

    added link to bootstrap stylesheet in public/index.html and created 1st draft of Header

--- group-board-finder/src/components/App/index.js
+++ group-board-finder/src/components/App/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../Header/index.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Tailwind</h1>
+        <Header />
       </div>
     );
   }
