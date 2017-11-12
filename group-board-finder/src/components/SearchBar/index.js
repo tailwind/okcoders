8a47a842691a9e4432bdeea31c750ee9678130e7
Matt Woods <mattwoods21@gmail.com>
2017-11-12 14:56:11 -0600

    Converted input form to React-Bootstrap Component
    * Replaced elements with React Bootstrap Library components

--- group-board-finder/src/components/SearchBar/index.js
+++ group-board-finder/src/components/SearchBar/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
 // This styles the input parent div
 const divStyle = {
-  margin: '20', textAlign: 'center'
+  margin: '20', 
+  textAlign: 'center',
+  marginTop: '2rem'
 };
 
 class SearchBar extends Component {
@@ -14,19 +17,28 @@ class SearchBar extends Component {
   
 	render() {
 		return (
-			<div style={divStyle} className='col-md-6'  >
-				<input
-        className='form-control form-control-lg'
-				value={this.state.term}
-				onChange={event => this.onInputChange(event.target.value)} 
-        style={{Width: '75%'}}
-        />
+      <div style={divStyle} className='col-md-6'>
+        <form>
+        <FormGroup
+          controlId="SearchBarForm"
+          bsSize="large"
+        >
+          <ControlLabel>Search Group Boards</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.term}
+            placeholder="What Group Boards sound interesting to you?"
+            onChange={event => this.handleChange(event.target.value)} 
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+      </form>
 			</div>
 		);
 	}
 
 // Updates the input to a controlled component
-	onInputChange(term) {
+  handleChange(term) {
 		this.setState({term});
 	}
 }
