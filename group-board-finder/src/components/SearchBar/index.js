599d37944bbe5743d3fd661170c2c0bdc3f62e70
Matt Woods <mattwoods21@gmail.com>
2017-11-12 17:17:07 -0600

    Resolved merge conflicts from david-header branch

--- group-board-finder/src/components/SearchBar/index.js
+++ group-board-finder/src/components/SearchBar/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
+// This styles the input parent div
+const divStyle = {
+  margin: '20', textAlign: 'center'
+};
+
 class SearchBar extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
+  constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
   }
+  
+	render() {
+		return (
+			<div style={divStyle} className='col-md-6'  >
+				<input
+        className='form-control form-control-lg'
+				value={this.state.term}
+				onChange={event => this.onInputChange(event.target.value)} 
+        style={{Width: '75%'}}
+        />
+			</div>
+		);
+	}
+
+// Updates the input to a controlled component
+	onInputChange(term) {
+		this.setState({term});
+	}
 }
 
 export default SearchBar;
