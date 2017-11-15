e6f0440c1f74b117860fc2537c8a71cbfe9fb1be
Patrick Richardson <Patrick.W.Richardson-1@ou.edu>
2017-11-15 02:24:24 -0600

    Sketching out API structure and loaded data into index.js

--- group-board-finder/src/index.js
+++ group-board-finder/src/index.js
@@ -4,8 +4,13 @@ import './index.css';
 import App from './components/App/index';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import {loadBoardData} from './api/datamanager.js'
+
 ReactDOM.render((
 <Router>
   <App />
 </Router>)
 , document.getElementById('root'));
+
+const data = loadBoardData();
+console.log(data)
