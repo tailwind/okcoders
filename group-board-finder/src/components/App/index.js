f27e8072d9fcbf2b6f88e2c8097cdc33d9583970
David Scott <dscottjr99@yahoo.com>
2017-11-16 19:59:21 -0600

    before rebase

--- group-board-finder/src/components/App/index.js
+++ group-board-finder/src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header/index.js';
 import Hero from '../Hero/index.js';
+import { MenuItem } from 'react-bootstrap';
 import SearchBar from '../SearchBar/index';
 import SearchListing from '../SearchListing/index';
 import Footer from '../Footer/index';
@@ -8,10 +9,27 @@ import Footer from '../Footer/index';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
-  render() {
+
+buildNavDropDownCategoriesList() {
+
+    const categories = ['Popular', 'Everything', 'Gifts', 'Videos', 'Animals and pets', 'Architecture', 'Art', 'Cars and motorcycles',
+    'Celebrities', 'DIY and crafts', 'Design', 'Education', 'Entertainment', 'Food and drink', 'Gardening', 'Geek', 'Hair and beauty',
+  'Health and fitness', 'History', 'Holidays and event', 'Home decor', 'Humor', 'Illustrations and posters', 'Kids and parenting',
+  'Men\'s fashion', 'Outdoors', 'Photography', 'Products', 'Quotes', 'Science and nature', 'Sports', 'Tattoos', 'Technology',
+  'Travel', 'Weddings', 'Women\'s fashion'];
+
+    const categorieslist = categories.map((category, i) =>
+      // expression goes here:
+    <MenuItem eventKey={i}>{category}</MenuItem>
+    );
+
+    return categorieslist;
+  }
+
+render() {
     return (
       <div className="App">
-        <Header />
+        <Header buildDropDown={this.buildNavDropDownCategoriesList}/>
         <Hero />
         <SearchBar />
         <Switch>
