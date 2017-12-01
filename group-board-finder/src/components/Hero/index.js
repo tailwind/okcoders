3ca28fc9bb549f444d5287c524e89dba9781806a
David Scott <dscottjr99@yahoo.com>
2017-11-30 21:51:49 -0600

    starting css changes to hero

--- group-board-finder/src/components/Hero/index.js
+++ group-board-finder/src/components/Hero/index.js
@@ -2,40 +2,64 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 
+const divStyle ={
+    background: "url(" + "https://d31a41pz1c3dkr.cloudfront.net/img/heros/laptop-pinterest.indexed.jpg" + ")"
+}
+
+const jumbotronStyle = {
+  height: "500px",
+  backgroundColor: "rgba(0, 65, 110, 0.9)"
+};
+
+const textStyle = {
+  color: "white",
+  textAlign: "center"
+}
+
 class Hero extends Component {
   render() {
     return (
       <Switch>
 
-{/* Display Hero on homepage */}   
+        {/* Display Hero on homepage */}   
 
         <Route exact path="/" render={() => (
-          <Jumbotron>
-          <h1>Pinterest Group Board Finder</h1>
+          <div style={divStyle}>
+          <Jumbotron style={jumbotronStyle}>
+          <h1 style={textStyle}>Pinterest Group Board Finder</h1>
           <br />
-          <h2>Request to join any Group Board with a click</h2>
           <br />
-          <h3>The most complete and up-to-date directory of Pinterest Group Boards</h3>
+          <h2 style={textStyle}>Request to join any Group Board with a click</h2>
+          <br />
+          <br />
+          <h3 style={textStyle}>The most complete and up-to-date directory of Pinterest Group Boards</h3>
         </Jumbotron>
+        </div>
         )}/>
 
-{/* Display Hero on Category results page */}  
+        {/* Display Hero on Category results page */}  
+
         <Route exact path="/category/:term" render={(props) => (
           <Jumbotron>
           <h1>Explore Top {props.match.params.term} Pinterest Group Boards</h1>
           <br />
+          <br />
           <h2>Request to join any Group Board with a click</h2>
+          <br />
           <br />
           <h3>[number of boards] {props.match.params.term} Group Boards found!</h3>
         </Jumbotron>
         )}/>
 
-{/* Display Hero on keyword search results page */}  
-        <Route exact path="/results/:term" render={(props) => (
+        {/* Display Hero on keyword search results page */}  
+
+        <Route exact path="/search/:term" render={(props) => (
           <Jumbotron>
           <h1>Search Results: {props.match.params.term} Pinterest Group Boards</h1>
           <br />
+          <br />
           <h2>Request to join any Group Board with a click</h2>
+          <br />
           <br />
           <h3>[number of boards] Group Boards found related to {props.match.params.term}</h3>
         </Jumbotron>
